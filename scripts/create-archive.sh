a9e84c08c6927118718a2b4b6404cdb0e8482682
Alexey Mileev <alemileev@gmail.com>
2023-12-25 19:19:52 +0400

    Potential fix

--- scripts/create-archive.sh
+++ scripts/create-archive.sh
@@ -6,7 +6,7 @@ mkdir -p model_store
 torch-model-archiver \
 --model-name paraphrase-multilingual-MiniLM-L12-v2 \
 --version 1.0 \
---model-file my_model/pytorch_model.bin \
+--model-file my_model/model.safetensors \
 --handler handler.py  \
 --extra-files "my_model/config.json,my_tokenizer/special_tokens_map.json,my_tokenizer/tokenizer.json,my_tokenizer/tokenizer_config.json,my_tokenizer/unigram.json" \
 --export-path model_store
